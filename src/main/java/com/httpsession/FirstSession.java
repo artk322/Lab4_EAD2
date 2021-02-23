e2417a92b16dc2b966f7eb9be6caa39f0fad0749
ar.kentbayev <artur.kentbayev@gmail.com>
2021-02-23 22:54:00 +0600

    init

---
+++ src/main/java/com/httpsession/FirstSession.java
@@ -0,0 +1,31 @@
+package com.httpsession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class FirstSession extends HttpServlet {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    request.getRequestDispatcher("session.html").include(request, response);
+
+    String name = request.getParameter("name");
+    String password = request.getParameter("password");
+
+    if (password.equals("dimon321")) {
+      out.print("<br>Welcome, " + name);
+      HttpSession session = request.getSession();
+      session.setAttribute("name", name);
+    } else {
+      out.print("Sorry, username or password error!");
+      request.getRequestDispatcher("loginHttp.html").include(request, response);
+    }
+    out.close();
+  }
+}
